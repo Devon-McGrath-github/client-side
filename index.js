edcfb7a438a9c2ddd81ce314e4ea8f4fd4cfc88b
EDA <eda@mac-mini-5.local>
2016-06-07 14:59:48 +1200

    Updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function (err, data) {
   console.log(data) // in case you're curious
 
   document.body.innerHTML = example({
-    name: 'Bevon',
+    name: data.name,
     latitude: data.latitude
   })
 })
