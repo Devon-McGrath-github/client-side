0c515c30578341aa54e0f1165527a94111aba778
EDA <eda@mac-mini-5.local>
2016-06-07 15:26:00 +1200

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function (err, data) {
-  if (err) {
-    console.log(err)
-  }
+document.getElementById("butt").addEventListener("click", displayInfo)
 
-  var objectData = JSON.parse(data.body)
 
-  document.body.innerHTML = example({
-    name: 'Bevon',
-    latitude: objectData.latitude
+function displayInfo() {
+
+  xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    var objectData = JSON.parse(data.body)
+
+    document.body.innerHTML = example({
+      name: 'Bevon',
+      latitude: objectData.latitude
+    })
   })
-})
+}
